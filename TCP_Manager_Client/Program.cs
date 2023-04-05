5ebbe4470239a4ef5d4d7808691e2914bc94a24b
Sahib Shakhayev <sahib.shakhayev@hotmail.com>
2023-04-05 21:44:57 +0400

    Add files via upload

---
+++ TCP_Manager_Client/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using TCP_Manager_Client;
+
+var ip = IPAddress.Loopback;
+var port = 27001;
+
+var client = new TcpClient();
+client.Connect(ip, port);
+var stream = client.GetStream();
+var br = new BinaryReader(stream);
+var bw = new BinaryWriter(stream);
+Command command = null;
+string responce = null;
+
+while (true)
+{
+    Console.WriteLine("Write command or HELP: ");
+    var str = Console.ReadLine().ToUpper();
+    if (str == "HELP")
+    {
+        Console.WriteLine();
+        Console.WriteLine("Command list:");
+        Console.WriteLine(Command.ProccessList);
+        Console.WriteLine($"{Command.Run} <proccess_name>");
+        Console.WriteLine($"{Command.Kill} <proccess_name>");
+        Console.WriteLine($"HELP");
+        Console.ReadKey();
+        Console.Clear();
+        continue;
+    }
+    var input = str.Split(' ');
+    switch (input[0])
+    {
+        case Command.ProccessList:
+            command = new Command { Text = input[0] };
+            bw.Write(JsonSerializer.Serialize(command));
+            responce = br.ReadString();
+            var proccessList = JsonSerializer.Deserialize<string[]>(responce);
+            foreach (var proccessName in proccessList)
+            {
+                Console.WriteLine($"    {proccessName}");
+            }
+            Console.ReadKey();
+            Console.Clear();
+            break;
+
+        case Command.Kill:
+            command = new Command { Text = input[0], Param = input[1] };
+            bw.Write(JsonSerializer.Serialize(command));
+            responce = br.ReadString();
+            var answer = JsonSerializer.Deserialize<string>(responce);
+            Console.WriteLine(answer);
+            Console.ReadKey();
+            Console.Clear();
+            break;
+        case Command.Run:
+            command = new Command { Text = input[0], Param = input[1] };
+            bw.Write(JsonSerializer.Serialize(command));
+            responce = br.ReadString();
+            var answer2 = JsonSerializer.Deserialize<string>(responce);
+            Console.WriteLine(answer2);
+            Console.ReadKey();
+            Console.Clear();
+            break;
+
+    }
+
+
+}
+
---
+++ TCP_Manager_Server/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using TCP_Manager_Server;
+
+var ip = IPAddress.Loopback;
+var port = 27001;
+
+var listener = new TcpListener(ip, port);
+
+listener.Start();
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    var stream = client.GetStream();
+    var br = new BinaryReader(stream);
+    var bw  = new BinaryWriter(stream);
+
+    while (true)
+    {
+        var input = br.ReadString();
+        var command = JsonSerializer.Deserialize<Command>(input);
+
+        if (command == null) continue;
+
+        Console.WriteLine(command.Text);
+        Console.WriteLine(command.Param);
+        switch (command.Text)
+        {
+            case Command.ProccessList:
+                var proccesses = Process.GetProcesses();
+                var proccessNames = JsonSerializer.Serialize(proccesses.Select(p => p.ProcessName));
+                bw.Write(proccessNames);
+                break;
+            case Command.Kill:
+                var proccess = command.Param;
+                Process[] processes = Process.GetProcessesByName(proccess);
+                foreach (var proc in processes) { proc.Kill(); }
+                bw.Write(JsonSerializer.Serialize(new String($"Proccess: {proccess} killed! ")));
+                break;
+
+            case Command.Run:
+                var run = command.Param;
+
+                try
+                {
+                    Process.Start(run);
+                    
+                    bw.Write(JsonSerializer.Serialize(new String($"Proccess: {run} started! ")));
+
+
+                    
+                    
+                }
+                catch (Exception ex)
+                {
+
+                    bw.Write(JsonSerializer.Serialize(new String(ex.Message)));
+                };
+                
+
+                break;
+            default:
+                break;
+        }
+
+    }
+}
